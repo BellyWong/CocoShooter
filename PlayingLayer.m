c85e2f4575f99db16bb978af92bf2f1d9cdf7ec5
okamurayasuyuki <haradashinya@gmail.com>
2013-01-30 13:30:53 +0900

    move enemy

--- PlayingLayer.m
+++ PlayingLayer.m
@@ -9,6 +9,10 @@
 #import "PlayingLayer.h"
 
 @implementation PlayingLayer
+{
+    NSMutableArray *enemies;
+}
+
 
 +(CCScene *)scene
 {
@@ -26,16 +30,44 @@
 	return scene;
     
 }
+-(void)createEnemies
+{
+    enemies = [[NSMutableArray alloc] init];
+    int screenHeight = [[CCDirector sharedDirector] winSize].height;
+    int screenWidth = [[CCDirector sharedDirector] winSize].width;
+    // create 3 enemies.
+    for(int i = 0 ;i < 3;i++){
+        
+        CCSprite *enemy = [CCSprite spriteWithFile:@"enemy.png" rect:CGRectMake(0, 0, 64, 64)];
+        int enemyOffset = enemy.contentSize.height;
+        
+        
+        int randHeight = enemyOffset +  (arc4random() % (screenHeight - enemyOffset));
+        enemy.position = ccp(screenWidth + arc4random() % 200,randHeight);
+        enemy.rotation = 45;
+        
+        [self addChild:enemy];
+        [enemies addObject:enemy];
+    }
+    
+}
 -(void)onEnter
 {
     [super onEnter];
-    
-    CCSprite *enemy = [CCSprite spriteWithFile:@"enemy.png"];
-    enemy.position = ccp(100,100);
-    [self addChild:enemy];
+    [self createEnemies];
+    [self scheduleUpdate];
 
-    
-    
     NSLog(@"onEnter");
+}
+
+
+
+-(void)update:(ccTime *)time
+{
+    // moveEnemy
+    CGPoint moveOffSet = ccp(-1,0);
+    for (CCSprite *enemy in enemies){
+        enemy.position = ccpAdd(enemy.position, moveOffSet);
+    }
 }
 @end
